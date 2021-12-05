93a44bd8dc9762954b1c9e8ec9542539d86d855d
Cory Moore <cory@corymoore.com>
2021-12-04 23:41:13 -0600

    3/4 complete with header layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-gray-400 via-gray-600 to-gray-800 mix-blend-multiply bg-cover bg-top">
-      <header className="App-header"></header>
-    </div>
+    <header className="relative w-full h-screen header">
+      <div className="absolute w-1/12 logo-box top-10 left-10">
+        <img src={logo} alt="Corpus Christi Axe Haus Logo" className="logo" />
+      </div>
+      <div className="absolute w-full text-center header-box top-1/2 left-1/2">
+        <h1 className="text-center text-white uppercase heading-main">
+          <span className="block mb-5 text-5xl tracking-widest heading-primary">
+            Corpus Christi Axe Haus
+          </span>
+          <span className="block text-2xl tracking-widest tagline">
+            A virtual Vallhalla for legenday experiences
+          </span>
+        </h1>
+        <a
+          href="/"
+          className="inline-block transform hover:scale-110 motion-reduce:transform-none py-4 px-4 bg-gray-50 text-gray-500 rounded-2xl mt-6"
+        >
+          Book Your Experience
+        </a>
+      </div>
+    </header>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require("material-ui-colors");
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
@@ -5,6 +6,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         "header-img": "url('./AxeHausHome2.jpg')",
+      },
+      colors: { ...colors },
+      fontFamily: {
+        heading: ["Fredericka\\ The\\ Great", "serif"],
       },
     },
   },
